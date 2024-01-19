8d13f1b7f17c1484458be2790802a61172b9c319
J22T <jordan22taylor@gmail.com>
2024-01-19 15:51:58 -0800

    started over with the CustomButton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,20 +1,19 @@
 "use client";
 
+import { CustomButtonProps } from '@/types';
 import Image from 'next/image';
 
-import { CustomButtonProps } from '@/types';
-
-const CustomButton = ( { title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handeClick }: CustomButtonProps) => {
   return (
     <button
         disabled={false}
         type={"button"}
         className={`custom-btn ${containerStyles}`}
-        onClick={handleClick}
-      >
-      <span className={`flex-1`}>
+        onClick={handeClick}
+    >
+        <span className={`flex-1`}>
           {title}
-      </span>
+        </span >
     </button>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import Image from 'next/image';
-import CustomButton from '.';
+
+import CustomButton from './CustomButton';
 
 const Hero = () => {
-  const handleScroll = () = > {
+  const handleScroll = ()=> {
 
   }
+
   return (
     <div className="hero">
-        <div className="flex-1 pt-36 padding-x"></div>
+      <div className="flex-1 pt-36 padding-x">
           <h1 className="hero__title">
-            Find, book,  or rent a car -- quickly and easily!
+            Find, book, or rent a car -- quickly and easily!
           </h1>
 
           <p className="hero__subtitle">
@@ -20,15 +22,10 @@ const Hero = () => {
 
           <CustomButton
             title="Explore Cars"
-            containerStyles="bg-primary-blue-text-white rounded-full mt-10"
-            handleClick={handleScroll}
+            containerStyles="bg-primary-blue text-white rounded-full mt-10"
+            handeClick={handleScroll}
           />
-    <div className="hero__image-container">
-      <div className="hero__image">
-        <Image src="/hero.png" alt="hero" fill className="object-contain" />
       </div>
-
-    </div>
     </div>
   )
 }

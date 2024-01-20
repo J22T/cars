6c3bb7c6943f7335ff7f3daccab33fdf62467030
J22T <jordan22taylor@gmail.com>
2024-01-19 16:09:01 -0800

    hero images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
           <h1 className="hero__title">
-            Find, book, or rent a car -- quickly and easily!
+            Find, search, rent and buy easily and simply.
           </h1>
 
           <p className="hero__subtitle">
@@ -25,6 +25,13 @@ const Hero = () => {
             containerStyles="bg-primary-blue text-white rounded-full mt-10"
             handeClick={handleScroll}
           />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   )

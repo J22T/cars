636eb35306d62e0323c57edf7e70b0b7efc46491
J22T <jordan22taylor@gmail.com>
2024-01-23 13:51:49 -0800

    CarCard price per day

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,40 @@
-import { useState } from 'react'
+"use client";
 
-const CarCard = () => {
+import { useState } from 'react';
+import Image from 'next/image';
+
+import { CarProps } from '@/types';
+import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive
+   } = car;
+
+   const carRent = calculateCarRent(city_mpg, year);
+
   return (
-    <div>CarCard</div>
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">
+          $
+        </span>
+            {carRent}
+        <span className="self-end text-[14px] font-medium">
+            /day
+        </span>
+      </p>
+    </div>
   )
 }
 

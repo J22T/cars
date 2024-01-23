675367451d1ffb0fedb2948c66925d1c8d465a94
J22T <jordan22taylor@gmail.com>
2024-01-22 19:26:07 -0800

    API JavaScript fetch index.ts

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '267908a9d2msh8753664677c948fp149467jsn110a18aedaa1',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}

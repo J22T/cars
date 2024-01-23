bca20463c725e4c9fdd7799c6bfaac16020eab01
J22T <jordan22taylor@gmail.com>
2024-01-22 16:44:54 -0800

    filteredManufactuers

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import Image from 'next/image';
 import { Combobox, Transition } from '@headlessui/react'
 
+import { manufacturers } from '@/constants';
 import { SearchManufacturerProps } from '@/types'
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }:  SearchManufacturerProps) => {
-const [query, setQuery] = useState('')
+const [query, setQuery] = useState('');
+
+const filteredManufacturers =
+    query === ""
+        ?  manufacturers
+        : manufacturers.filter((item) => (
+            item.toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+g,
+            "")
+    ))
 
   return (
     <div className="search-manufacturer">
@@ -28,6 +39,18 @@ const [query, setQuery] = useState('')
                     displayValue={(manufacturer: string) => manufacturer}
                     onChange={(e) => setQuery(e.target.value)}
                     />
+
+                    <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    afterLeave={() => setQuery ('')}
+                    >
+                        <Combobox.Options>
+
+                        </Combobox.Options>
+                    </Transition>
             </div>
         </Combobox>
     </div>

33a82cb8ad6d236672af61037ff420b2b190e12e
J22T <jordan22taylor@gmail.com>
2024-01-27 19:41:27 -0800

    search parameters; globals.css

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -27,4 +27,4 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles
   );
 };
 
-export default CustomButton
+export default CustomButton;
--- utils/index.ts
+++ utils/index.ts
@@ -10,10 +10,15 @@ export async function fetchCars(filters: FilterProps) {
             'X-RapidAPI-Host' : 'cars-by-api-ninjas.p.rapidapi.com'
         }
 
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}$limit=${limit}&fuel_type=${fuel}`, {
+        // Set the required headers for the API request
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}$limit=${limit}&fuel_type=${fuel}`,
+        {
             headers: headers,
-        });
+        }
+        );
 
+        // Parse the response as JSON
         const result = await response.json();
 
         return result;
